ada3e5b60d8d668b61b033ed034c8ea540b79201
flyingpigs77 <137256640+flyingpigs77@users.noreply.github.com>
2024-05-22 14:11:55 +0000

    testing

--- script.js
+++ script.js
@@ -1,82 +1,60 @@
 const scene = new THREE.Scene();
-
-//REMINDER: FIGURE OUT WHY WORKS ON REPLIT AND NOT GITHUB
-//^^^^^^^^^^^^
-
-// The camera
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   .1,
   1000
 );
-
-// Make the camera further from the cube
-
-camera.position.z = 8;
-var zloc = 8;
-camera.position.x = 0;
-var xloc = 0;
-camera.position.y = 1;
-var yloc = 1;
-
-camera.lookAt(new THREE.Vector3(0,0,0));
-
-// The renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x000000, 1);
 document.body.appendChild(renderer.domElement);
 
-// cube
+camera.position.z = 8;
+var zloc = camera.position.z;
+camera.position.x = 0;
+var xloc = camera.position.x;;
+camera.position.y = 1;
+var yloc = camera.position.y;
+camera.lookAt(new THREE.Vector3(0,0,1));
+
+
+
+
 const cube = {
-  // geometry: cubes shape and size
-  geometry: new THREE.BoxGeometry(1, 1, 1),
-  // material: color/appearance
+  geometry: new THREE.BoxGeometry(1, .2, 1),
   material: new THREE.MeshBasicMaterial({ color: 0x00ff00})
 };
-
-const wireFrame = {
-  geometry: new THREE.EdgesGeometry( cube.geometry ),
-  material: new THREE.LineBasicMaterial( { color: 0x000000 })
-}
-
-
-// mesh: combines the geometry and material so you can add them to the scene as one
-// cube.mesh = new THREE.Mesh(cube.geometry, cube.material);
 cube.mesh = new THREE.Mesh(cube.geometry, cube.material);
 
+const wireFrame = {
+  geometry: new THREE.EdgesGeometry(cube.geometry),
+  material: new THREE.LineBasicMaterial( {color: 0x000000 
+  })
+}
 wireFrame.mesh = new THREE.LineSegments(wireFrame.geometry, wireFrame.material);
 
+
+
+
 var newMesh = cube.mesh.clone();
-newMesh.position.set(0,0,-2);
-
+newMesh.position.set(0,0,-1);
 var newFrame = wireFrame.mesh.clone();
-newFrame.position.set(0,0,-2);
+newFrame.position.set(0,0,-1);
 
-const group = new THREE.Group();
+
+
+const group = new THREE.Object3D();
 group.add(cube.mesh);
 group.add(wireFrame.mesh);
 group.add(newMesh);
 group.add(newFrame);
-group.rotation.x = Math.PI/8;
+group.rotation.x = Math.PI/12;
 
-scene.add(group);
-
-group.position.x = 0;
-group.position.y = 0;
-group.position.z = 0;
-
-var bgCube = cube.mesh.clone();
-var bgFrame = wireFrame.mesh.clone();
-
-const bgGroup = new THREE.Group();
-bgGroup.add(bgCube);
-bgGroup.add(bgFrame);
-// bgGroup.geometry.set(1,1,1);
-bgGroup.position.set(3,0,-5);
-
-scene.add(bgGroup);
+var pivot = new THREE.Object3D();
+scene.add(pivot);
+pivot.add(group)
+group.position.set(0, 0, 2/4); // the negative of the average of all the objects positions per axis 
 
 
 var req;
@@ -127,13 +105,8 @@ function updateCameraPosition() {
 }
 
 function render() {
-  // Renders scene and camera
   renderer.render(scene, camera);
-
-  //Rotate the cube every frame
-  group.rotation.y += 0.05;
-
-  // calls every 1/60 of secoind
+  pivot.rotation.y += 0.01;
   requestAnimationFrame(render);
 }
 render();
